16cd1d9de1d77831c652613bc25e420c54147a87
Maurício Naoki <naokimau@gmail.com>
2025-02-15 23:08:54 -0300

    fix: alteração no espaçamento do formulario

--- src/pages/minhasFinancas/MinhasFinancas.tsx
+++ src/pages/minhasFinancas/MinhasFinancas.tsx
@@ -257,80 +257,87 @@ export const MinhasFinancas = () => {
             onClose={() => setAddBalanceModal(false)}
           >
             <form onSubmit={handleSubmit(handleSubmitForm)}>
-              <Controller
-                name="type"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <ToggleButtonGroup
-                    color="primary"
-                    value={field.value}
-                    exclusive
-                    onChange={(_, newValue) => field.onChange(newValue)}
-                    sx={{ gap: "1rem" }}
-                  >
-                    <ToggleButton
-                      value="adicionar"
-                      sx={{
-                        border: "2px solid #ccc ",
-                        borderRadius: "8px !important",
-                        "&.Mui-selected": {
-                          backgroundColor: "#4caf50",
-                          borderColor: "#4caf50",
-                          color: "white",
-                        },
-                        "&.Mui-selected:hover": {
-                          backgroundColor: "#4caf50",
-                        },
-                      }}
-                    >
-                      Adicionar
-                    </ToggleButton>
-                    <ToggleButton
-                      value="descontar"
-                      sx={{
-                        border: "2px solid #ccc !important",
-                        borderRadius: "8px !important",
-                        "&.Mui-selected": {
-                          backgroundColor: "#f44336",
-                          borderColor: "#f44336 !important",
-                          color: "white",
-                        },
-                        "&.Mui-selected:hover": {
-                          backgroundColor: "#f44336",
-                        },
-                      }}
-                    >
-                      Descontar
-                    </ToggleButton>
-                  </ToggleButtonGroup>
-                )}
-              />
-              <Input
-                {...register("value")}
-                sx={{ fontSize: "2rem" }}
-                startAdornment={
-                  <InputAdornment position="start">
-                    <Typography fontSize="2rem">
-                      {EditBalance === "adicionar" ? "+" : "-"} R$
-                    </Typography>
-                  </InputAdornment>
-                }
-              />
-              <Button
-                type="submit"
-                size="large"
-                variant="contained"
-                disableElevation
-                sx={{
-                  borderRadius: "1rem",
-                  maxWidth: "12rem",
-                  width: "100%",
-                  alignSelf: "center",
-                }}
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                flexDirection="column"
+                gap="2rem"
               >
-                Salvar
-              </Button>
+                <Controller
+                  name="type"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <ToggleButtonGroup
+                      color="primary"
+                      value={field.value}
+                      exclusive
+                      onChange={(_, newValue) => field.onChange(newValue)}
+                      sx={{ gap: "1rem" }}
+                    >
+                      <ToggleButton
+                        value="adicionar"
+                        sx={{
+                          border: "2px solid #ccc ",
+                          borderRadius: "8px !important",
+                          "&.Mui-selected": {
+                            backgroundColor: "#4caf50",
+                            borderColor: "#4caf50",
+                            color: "white",
+                          },
+                          "&.Mui-selected:hover": {
+                            backgroundColor: "#4caf50",
+                          },
+                        }}
+                      >
+                        Adicionar
+                      </ToggleButton>
+                      <ToggleButton
+                        value="descontar"
+                        sx={{
+                          border: "2px solid #ccc !important",
+                          borderRadius: "8px !important",
+                          "&.Mui-selected": {
+                            backgroundColor: "#f44336",
+                            borderColor: "#f44336 !important",
+                            color: "white",
+                          },
+                          "&.Mui-selected:hover": {
+                            backgroundColor: "#f44336",
+                          },
+                        }}
+                      >
+                        Descontar
+                      </ToggleButton>
+                    </ToggleButtonGroup>
+                  )}
+                />
+                <Input
+                  {...register("value")}
+                  sx={{ fontSize: "2rem" }}
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <Typography fontSize="2rem">
+                        {EditBalance === "adicionar" ? "+" : "-"} R$
+                      </Typography>
+                    </InputAdornment>
+                  }
+                />
+                <Button
+                  type="submit"
+                  size="large"
+                  variant="contained"
+                  disableElevation
+                  sx={{
+                    borderRadius: "1rem",
+                    maxWidth: "12rem",
+                    width: "100%",
+                    alignSelf: "center",
+                  }}
+                >
+                  Salvar
+                </Button>
+              </Box>
             </form>
           </CModal>
         </Grid>
